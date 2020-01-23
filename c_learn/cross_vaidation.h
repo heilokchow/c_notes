aacb84554eea47ec697c93e71fea7f140faae49c
heilokchow <u3514104@connect.hku.hk>
2020-01-23 23:54:11 +0800

    openmp

--- c_learn/cross_vaidation.h
+++ c_learn/cross_vaidation.h
@@ -91,4 +91,34 @@ void cross_validation_bench(const std::shared_ptr<Linear_regression> x) {
     } while (_cross_num > 0);
 }
 
+void cross_validation_bench_omp(const std::shared_ptr<Linear_regression> x, int ii) {
+
+    int n = x->n, p = x->p, curr = ii;
+    int n1 = n / 10, n2 = 9 * n1, n3 = 2 * n;
+    std::random_device device;
+    std::mt19937 generator(device());
+    std::normal_distribution<double> normal(0.0, 1.0);
+    VectorXd e(n3), y(n3), xy(p), beta(p);
+    double err = 0;
+
+    for (size_t i = 0; i < n; i++) {
+        e(i) = normal(generator);
+        e(i + n) = e(i);
+    }
+    y = x->b + e;
+
+    for (size_t i = 0; i < 10; i++) {
+        Map<VectorXd, 0, InnerStride<1>> Y1(&y(i * n1), n1);
+        Map<VectorXd, 0, InnerStride<1>> Y2(&y((i + 1) * n1), n2);
+        Map<VectorXd, 0, InnerStride<1>> E1(&e(i * n1), n1);
+        Map<VectorXd, 0, InnerStride<1>> E2(&e((i + 1) * n1), n2);
+        Map<MatrixXd, 0, Stride<Dynamic, 1>> X1(&x->X(i * n1, 0), n1, p, Stride<Dynamic, 1>(n3, 1));
+        Map<MatrixXd, 0, Stride<Dynamic, 1>> X2(&x->X((i + 1) * n1, 0), n2, p, Stride<Dynamic, 1>(n3, 1));
+        xy.noalias() = X2.transpose() * Y2;
+        beta.noalias() = x->XX_cross[i].llt().solve(xy);
+        err += (Y1 - X1 * beta).sum();
+    }
+    std::cout << curr << ", Average error: " << err / n << std::endl;
+}
+
 #endif
--- c_learn/main.cpp
+++ c_learn/main.cpp
@@ -3,13 +3,60 @@
 #include <vector>
 #include <thread>
 #include <mutex>
+#include "omp.h"
 
 #include "cross_vaidation.h"
 #include "timer.h"
 
+static long num_steps = 100000000;
+double step;
+#define NUM_THREADS 8
 
 int main()
 {
+    auto stat = std::make_shared<Linear_regression>(10000, 500);
+    
+    omp_set_num_threads(NUM_THREADS);
+
+
+    {
+        Timer t;
+#pragma omp parallel for schedule (dynamic)
+    for (int i = 0; i < 100; i++) {
+        MatrixXd X = MatrixXd::Random(2000, 2000);
+        MatrixXd Y = MatrixXd::Random(2000, 2000);
+        MatrixXd C;
+        C.noalias() = X * Y;
+        printf("pos: %d\n", i);
+        }
+    }
+
+    /*OMP guide code
+    step = 1.0 / static_cast<double>(num_steps);
+    double pi = 0.0, sum = 0.0;
+    omp_set_num_threads(NUM_THREADS);
+
+    double start = omp_get_wtime();
+#pragma omp parallel
+    {
+        int i = omp_get_thread_num(), j;
+        int k = omp_get_num_threads();
+        printf("%d, %d\n", i, k);
+        double x, sum_par = 0.0;
+        for ( j = i; j < num_steps; j = j + k) {
+            x = (j + 0.5) * step;
+            sum_par += 4.0 / (1.0 + x * x);
+        }
+#pragma omp critical
+        sum += sum_par;
+    }
+    double end = omp_get_wtime();
+
+    pi = sum * step;
+
+    printf("%f, Pi: %.6f", end - start, pi);*/
+
+    /*Use std::thread for multuithreading
     auto stat = std::make_shared<Linear_regression>(100,10);
   
     std::array<std::thread, 8> workers;
@@ -21,6 +68,7 @@ int main()
         
         for (auto& t : workers)
             t.join();
-    }
+    }*/
+
     return 0;
 }
