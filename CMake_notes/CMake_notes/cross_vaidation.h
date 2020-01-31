3751bfb6e57448d27257f220586416dd3ed2b2dd
heilokchow <u3514104@connect.hku.hk>
2020-01-31 21:36:08 +0800

    eigen+cmake

---
+++ CMake_notes/CMake_notes/cross_vaidation.h
@@ -0,0 +1,124 @@
+#ifndef CROSS_VALIDATION_H
+#define CROSS_VALIDATION_H
+
+#define EIGEN_DONT_PARALLELIZE 
+#include <iostream>
+#include <eigen3/Eigen/Dense>
+#include <thread>
+#include <mutex>
+#include <random>
+#include <memory>
+#include "timer.h"
+
+using namespace Eigen;
+#ifndef CROSS_NUM
+#define CROSS_NUM 100
+#endif
+
+std::mutex iomutex;
+int _cross_num = CROSS_NUM;
+
+struct Linear_regression {
+public:
+    MatrixXd X;
+    VectorXd b;
+    std::array<MatrixXd, 10> XX_cross;
+    int n, p;
+
+    Linear_regression(int nn, int pp) :
+        n(nn), p(pp), X(2*nn,pp), b(2*nn) {
+        int n1 = n / 10, n2 = 9 * n1, n3 = 2 * n;
+        MatrixXd XX = MatrixXd::Zero(p, p);
+        std::random_device device;
+        std::mt19937 generator(device());
+        std::normal_distribution<double> normal(0.0, 1.0);
+        {
+            Timer t0;
+            for (size_t i = 0; i < p; i++) {
+                for (size_t j = 0; j < n; j++) {
+                    X(j, i) = normal(generator);
+                    X(j + n, i) = X(j, i);
+                }
+            }
+        }
+
+        for (size_t i = 0; i < 10; i++) {
+            Map<MatrixXd, 0, Stride<Dynamic, 1>> X1(&X(i * n1, 0), n1, p, Stride<Dynamic, 1>(n3, 1));
+            XX_cross[i].resize(p, p);
+            XX_cross[i] = X1.transpose() * X1;
+            XX += XX_cross[i];
+        }
+
+        for (size_t i = 0; i < 10; i++)
+            XX_cross[i] = XX - XX_cross[i];
+        b = X.rowwise().sum();
+    }
+};
+
+void cross_validation_bench(const std::shared_ptr<Linear_regression> x) {
+
+    int n = x->n, p = x->p, curr = 0;
+    int n1 = n / 10, n2 = 9 * n1, n3 = 2 * n;
+    std::random_device device;
+    std::mt19937 generator(device());
+    std::normal_distribution<double> normal(0.0, 1.0);
+    VectorXd e(n3), y(n3), xy(p), beta(p);
+    do
+    {
+        double err = 0;
+        iomutex.lock();
+        curr = _cross_num--;
+        iomutex.unlock();
+
+        for (size_t i = 0; i < n; i++) {
+            e(i) = normal(generator);
+            e(i + n) = e(i);
+        }
+        y = x->b + e;
+
+        for (size_t i = 0; i < 10; i++) {
+            Map<VectorXd, 0, InnerStride<1>> Y1(&y(i * n1), n1);
+            Map<VectorXd, 0, InnerStride<1>> Y2(&y((i + 1)* n1), n2);
+            Map<VectorXd, 0, InnerStride<1>> E1(&e(i * n1), n1);
+            Map<VectorXd, 0, InnerStride<1>> E2(&e((i + 1) * n1), n2);
+            Map<MatrixXd, 0, Stride<Dynamic, 1>> X1(&x->X(i * n1, 0), n1, p, Stride<Dynamic, 1>(n3, 1));
+            Map<MatrixXd, 0, Stride<Dynamic, 1>> X2(&x->X((i + 1) * n1, 0), n2, p, Stride<Dynamic, 1>(n3, 1));
+            xy.noalias() = X2.transpose() * Y2;
+            beta.noalias() = x->XX_cross[i].llt().solve(xy);
+            err += (Y1 - X1 * beta).sum();
+        }
+        std::cout << curr << ", Average error: " << err / n << std::endl;
+    } while (_cross_num > 0);
+}
+
+void cross_validation_bench_omp(const std::shared_ptr<Linear_regression> x, int ii) {
+
+    int n = x->n, p = x->p, curr = ii;
+    int n1 = n / 10, n2 = 9 * n1, n3 = 2 * n;
+    std::random_device device;
+    std::mt19937 generator(device());
+    std::normal_distribution<double> normal(0.0, 1.0);
+    VectorXd e(n3), y(n3), xy(p), beta(p);
+    double err = 0;
+
+    for (size_t i = 0; i < n; i++) {
+        e(i) = normal(generator);
+        e(i + n) = e(i);
+    }
+    y = x->b + e;
+
+    for (size_t i = 0; i < 10; i++) {
+        Map<VectorXd, 0, InnerStride<1>> Y1(&y(i * n1), n1);
+        Map<VectorXd, 0, InnerStride<1>> Y2(&y((i + 1) * n1), n2);
+        Map<VectorXd, 0, InnerStride<1>> E1(&e(i * n1), n1);
+        Map<VectorXd, 0, InnerStride<1>> E2(&e((i + 1) * n1), n2);
+        Map<MatrixXd, 0, Stride<Dynamic, 1>> X1(&x->X(i * n1, 0), n1, p, Stride<Dynamic, 1>(n3, 1));
+        Map<MatrixXd, 0, Stride<Dynamic, 1>> X2(&x->X((i + 1) * n1, 0), n2, p, Stride<Dynamic, 1>(n3, 1));
+        xy.noalias() = X2.transpose() * Y2;
+        beta.noalias() = x->XX_cross[i].llt().solve(xy);
+        err += (Y1 - X1 * beta).sum();
+    }
+    std::cout << curr << ", Average error: " << err / n << std::endl;
+}
+
+#endif
--- CMake_notes/CMake_notes/main.cpp
+++ CMake_notes/CMake_notes/main.cpp
@@ -16,6 +16,7 @@ double step;
 
 int main()
 {
+    /*OMP PI Example
     double pi = 0.0, sum = 0.0;
     double step = 1.0 / static_cast<double>(num_steps);
 
@@ -34,9 +35,8 @@ int main()
     double end = omp_get_wtime();
 
     pi = sum * step;
-    printf("%f, Pi: %.6f", end - start, pi);
+    printf("%f, Pi: %.6f\n", end - start, pi);*/
 
-    /*OMP guide code Module 5
     auto stat = std::make_shared<Linear_regression>(4000, 500);
     
     omp_set_num_threads(NUM_THREADS);
@@ -44,11 +44,11 @@ int main()
 
     {
         Timer t;
-#pragma omp parallel for schedule (dynamic)
+        #pragma omp parallel for schedule (dynamic)
         for (int i = 0; i < 100; i++) {
             cross_validation_bench_omp(stat, i);
         }
-    }*/
+    }
 
     /*OMP guide code Module 1
     step = 1.0 / static_cast<double>(num_steps);
@@ -94,99 +94,74 @@ int main()
 
 #else
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <omp.h>
-
-#ifndef N
-#define N 20
+#ifdef APPLE
+#include <stdlib.h>
+#else
+#include <memory>
 #endif
-#ifndef FS
-#define FS 25
-#endif
+#include <stdio.h>
 
-struct node {
-    int data;
-    int fibdata;
-    struct node* next;
-};
+#define N        10000000
 
-int fib(int n) {
-    int x, y;
-    if (n < 2) {
-        return (n);
-    }
-    else {
-        x = fib(n - 1);
-        y = fib(n - 2);
-        return (x + y);
-    }
-}
+/* Some random number constants from numerical recipies */
+#define SEED       2531
+#define RAND_MULT  1366
+#define RAND_ADD   150889
+#define RAND_MOD   714025
+int randy = SEED;
 
-void processwork(struct node* p)
+/* function to fill an array with random numbers */
+void fill_rand(int length, double* a)
 {
-    int n;
-    n = p->data;
-    p->fibdata = fib(n);
-}
-
-struct node* init_list(struct node* p) {
     int i;
-    struct node* head = NULL;
-    struct node* temp = NULL;
-
-    head = (node*)malloc(sizeof(struct node));
-    p = head;
-    p->data = FS;
-    p->fibdata = 0;
-    for (i = 0; i < N; i++) {
-        temp = (node*)malloc(sizeof(struct node));
-        p->next = temp;
-        p = temp;
-        p->data = FS + i + 1;
-        p->fibdata = i + 1;
+    for (i = 0; i < length; i++) {
+        randy = (RAND_MULT * randy + RAND_ADD) % RAND_MOD;
+        *(a + i) = ((double)randy) / ((double)RAND_MOD);
     }
-    p->next = NULL;
-    return head;
 }
 
-int main(int argc, char* argv[]) {
-    double start, end;
-    struct node* p = NULL;
-    struct node* temp = NULL;
-    struct node* head = NULL;
+/* function to sum the elements of an array */
+double Sum_array(int length, double* a)
+{
+    int i;  double sum = 0.0;
+    for (i = 0; i < length; i++)  sum += *(a + i);
+    return sum;
+}
 
-    printf("Process linked list\n");
-    printf("  Each linked list node will be processed by function 'processwork()'\n");
-    printf("  Each ll node will compute %d fibonacci numbers beginning with %d\n", N, FS);
+int main()
+{
+    double* A, sum, runtime;
+    int flag = 0;
 
-    p = init_list(p);
-    head = p;
-    
-    start = omp_get_wtime();
-#pragma omp parallel
-#pragma omp single
+    A = (double*)malloc(N * sizeof(double));
+
+    runtime = omp_get_wtime();
+
+    #pragma omp parallel sections
     {
-        for (temp = head; temp; temp = temp->next)
-#pragma omp task firstprivate(temp)
-            processwork(temp);
+        #pragma omp section
+        {
+            fill_rand(N, A);        // Producer: fill an array of data
+            #pragma omp flush
+            flag = 1;
+            #pragma omp flush (flag)
+        }
+
+        #pragma omp section
+        {
+            #pragma omp flush (flag)
+            while (flag == 0) {
+                #pragma omp flush (flag)
+            }
+            #pragma omp flush
+            sum = Sum_array(N, A);  // Consumer: sum the array
+        }
     }
-    end = omp_get_wtime();
-    p = head;
-    while (p != NULL) {
-        printf("%d : %d\n", p->data, p->fibdata);
-        temp = p->next;
-        free(p);
-        p = temp;
-    }
-    free(p);
 
-    printf("Compute Time: %f seconds\n", end - start);
+    runtime = omp_get_wtime() - runtime;
 
-    printf("Hello World\n");
-
-    return 0;
+    printf(" In %f seconds, The sum is %f \n", runtime, sum);
 }
-
 
 #endif
